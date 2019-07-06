124a9883bcf737190a190818fd9ac0ad8f5e1e72
Skoze <1034217469@qq.com>
2019-07-07 02:25:27 +0800

    homework1 done

--- app/src/main/java/nju/androidchat/client/component/ItemTextReceive.java
+++ app/src/main/java/nju/androidchat/client/component/ItemTextReceive.java
@@ -3,6 +3,12 @@ package nju.androidchat.client.component;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -10,6 +16,7 @@ import android.widget.TextView;
 
 import androidx.annotation.StyleableRes;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import nju.androidchat.client.R;
@@ -25,6 +32,7 @@ public class ItemTextReceive extends LinearLayout {
     private UUID messageId;
     private OnRecallMessageRequested onRecallMessageRequested;
 
+    private final static String img_pattern = "!\\[.+\\]\\(.+\\)";
 
     public ItemTextReceive(Context context, String text, UUID messageId) {
         super(context);
@@ -44,6 +52,34 @@ public class ItemTextReceive extends LinearLayout {
     }
 
     public void setText(String text) {
-        textView.setText(text);
+        if (text.matches(img_pattern)) {
+            Runnable runnable = () -> {
+                SpannableString spannableString = new SpannableString("   ");
+                String url = text.substring(text.indexOf("(") + 1, text.length() - 1);
+                System.out.println(url);
+                try {
+                    Drawable drawable = Drawable.createFromStream(new java.net.URL(url).openStream(), null);
+                    drawable.setBounds(0, 0, drawable.getIntrinsicWidth() * 5, drawable.getIntrinsicHeight() * 5);
+                    spannableString.setSpan(new ImageSpan(drawable), 1, 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Message msg = new Message();
+                msg.obj = spannableString;
+                handler.sendMessage(msg);
+            };
+            String str = text.substring(text.indexOf("[") + 1, text.indexOf("]"));
+            textView.setText(str);
+            new Thread(runnable).start();
+        } else {
+            textView.setText(text);
+        }
+
     }
+
+    private Handler handler = new Handler(msg -> {
+        SpannableString spannableString = (SpannableString) msg.obj;
+        textView.setText(spannableString);
+        return true;
+    });
 }
--- app/src/main/java/nju/androidchat/client/component/ItemTextSend.java
+++ app/src/main/java/nju/androidchat/client/component/ItemTextSend.java
@@ -2,14 +2,19 @@ package nju.androidchat.client.component;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.StyleableRes;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import lombok.Setter;
@@ -22,7 +27,10 @@ public class ItemTextSend extends LinearLayout implements View.OnLongClickListen
     private TextView textView;
     private Context context;
     private UUID messageId;
-    @Setter private OnRecallMessageRequested onRecallMessageRequested;
+    @Setter
+    private OnRecallMessageRequested onRecallMessageRequested;
+
+    private final static String img_pattern = "!\\[.+\\]\\(.+\\)";
 
     public ItemTextSend(Context context, String text, UUID messageId, OnRecallMessageRequested onRecallMessageRequested) {
         super(context);
@@ -41,8 +49,36 @@ public class ItemTextSend extends LinearLayout implements View.OnLongClickListen
     }
 
     public void setText(String text) {
-        textView.setText(text);
+        if (text.matches(img_pattern)) {
+            Runnable runnable = () -> {
+                SpannableString spannableString = new SpannableString("   ");
+                String url = text.substring(text.indexOf("(") + 1, text.length() - 1);
+                System.out.println(url);
+                try {
+                    Drawable drawable = Drawable.createFromStream(new java.net.URL(url).openStream(), null);
+                    drawable.setBounds(0, 0, drawable.getIntrinsicWidth() * 5, drawable.getIntrinsicHeight() * 5);
+                    spannableString.setSpan(new ImageSpan(drawable), 1, 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Message msg = new Message();
+                msg.obj = spannableString;
+                handler.sendMessage(msg);
+            };
+            String str = text.substring(text.indexOf("[") + 1, text.indexOf("]"));
+            textView.setText(str);
+            new Thread(runnable).start();
+        } else {
+            textView.setText(text);
+        }
+
     }
+
+    private Handler handler = new Handler(msg -> {
+        SpannableString spannableString = (SpannableString) msg.obj;
+        textView.setText(spannableString);
+        return true;
+    });
 
     @Override
     public boolean onLongClick(View v) {
